4ee67c5b003f7709566860eaf4af17624a0be7c4
Kevin <k1den@rambler.ru>
2024-01-14 23:27:38 +0300

    Second commit

--- src/main/java/ru/netology/controller/SQLjdbcDaoLayerController.java
+++ src/main/java/ru/netology/controller/SQLjdbcDaoLayerController.java
@@ -3,23 +3,15 @@ package ru.netology.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.netology.repository.SQLdaoRepository;
 
-import java.sql.SQLException;
 import java.util.List;
-
-import ru.netology.repository.SQLdaoRepositoryImpl;
 
 @RestController
 
 public class SQLjdbcDaoLayerController {
-    SQLdaoRepositoryImpl repository;
-
-    SQLjdbcDaoLayerController(SQLdaoRepositoryImpl repository) {
-        this.repository = repository;
-    }
-
     @GetMapping("/products/fetch-product")
-    public List<String> getProductName(@RequestParam("name") String name) throws SQLException {
-        return repository.getProductName(name);
+    public List<String> getProductName(@RequestParam("name") String name) {
+        return SQLdaoRepository.getProductName(name);
     }
 }
--- src/main/java/ru/netology/repository/SQLdaoRepository.java
+++ src/main/java/ru/netology/repository/SQLdaoRepository.java
@@ -1,4 +1,36 @@
 package ru.netology.repository;
 
-public interface SQLdaoRepository {
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@org.springframework.stereotype.Repository
+
+public class SQLdaoRepository {
+    private static final String request = read("product_name.sql");
+    private static NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public SQLdaoRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        SQLdaoRepository.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    public static List<String> getProductName(String name) {
+        return namedParameterJdbcTemplate.queryForList(request, Map.of("name", name), String.class);
+    }
+
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
